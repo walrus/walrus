6f14589340321f3f509c3027cc1d5c9af987250d
Daniel Clay <dclay.daniel@gmail.com>
2017-04-11 15:12:14 +0100

    Add autocalibration of IMU before starting logging.

--- curie/src/logger/logger.ino
+++ curie/src/logger/logger.ino
@@ -22,49 +22,12 @@ void setup() {
   /* Initialise the IMU */
   CurieIMU.begin();
   CurieIMU.attachInterrupt(eventCallback);
-  
-  if (calibrateOffsets) {
-    Serial.println("Internal sensor offsets BEFORE calibration...");
-    Serial.print(CurieIMU.getAccelerometerOffset(X_AXIS));
-    Serial.print("\t"); // -76
-    Serial.print(CurieIMU.getAccelerometerOffset(Y_AXIS));
-    Serial.print("\t"); // -235
-    Serial.print(CurieIMU.getAccelerometerOffset(Z_AXIS));
-    Serial.print("\t"); // 168
-    Serial.print(CurieIMU.getGyroOffset(X_AXIS));
-    Serial.print("\t"); // 0
-    Serial.print(CurieIMU.getGyroOffset(Y_AXIS));
-    Serial.print("\t"); // 0
-    Serial.println(CurieIMU.getGyroOffset(Z_AXIS));
 
-    Serial.println("About to calibrate. Make sure your board is stable and upright");
-    delay(1000);
-
-    // The board must be resting in a horizontal position for
-    // the following calibration procedure to work correctly!
-    Serial.print("Starting Gyroscope calibration and enabling offset compensation...");
-    CurieIMU.autoCalibrateGyroOffset();
-    Serial.println(" Done");
-
-    Serial.print("Starting Acceleration calibration and enabling offset compensation...");
-    CurieIMU.autoCalibrateAccelerometerOffset(X_AXIS, 0);
-    CurieIMU.autoCalibrateAccelerometerOffset(Y_AXIS, 0);
-    CurieIMU.autoCalibrateAccelerometerOffset(Z_AXIS, 1);
-    Serial.println(" Done");
-
-    Serial.println("Internal sensor offsets AFTER calibration...");
-    Serial.print(CurieIMU.getAccelerometerOffset(X_AXIS));
-    Serial.print("\t"); // -76
-    Serial.print(CurieIMU.getAccelerometerOffset(Y_AXIS));
-    Serial.print("\t"); // -2359
-    Serial.print(CurieIMU.getAccelerometerOffset(Z_AXIS));
-    Serial.print("\t"); // 1688
-    Serial.print(CurieIMU.getGyroOffset(X_AXIS));
-    Serial.print("\t"); // 0
-    Serial.print(CurieIMU.getGyroOffset(Y_AXIS));
-    Serial.print("\t"); // 0
-    Serial.println(CurieIMU.getGyroOffset(Z_AXIS));
-  }
+  /* Calibrate the IMU's gyrometer and accelerometer */
+  CurieIMU.autoCalibrateGyroOffset();
+  CurieIMU.autoCalibrateAccelerometerOffset(X_AXIS, 0);
+  CurieIMU.autoCalibrateAccelerometerOffset(Y_AXIS, 0);
+  CurieIMU.autoCalibrateAccelerometerOffset(Z_AXIS, 1);
   
   /* Enable Motion Detection */
   CurieIMU.setDetectionThreshold(CURIE_IMU_MOTION, 25); // mg
