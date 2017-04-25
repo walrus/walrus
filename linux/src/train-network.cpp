ce88df0480a6259e44e658c628a7ebea5ac9c9a9
Daniel Clay <dclay.daniel@gmail.com>
2017-04-25 20:39:01 +0100

    Start on training program

---
+++ linux/src/train-network.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+
+#include "../../network/src/network.hpp"
+
+Network loadNetwork(string filename) {
+    // Assumes that the file is located in the folder '../../network/configurations'
+    return new Network;
+}
+
+void saveNetwork(Network network, string filename) {
+    // Saves the file in the folder '../../network/configurations'
+}
+
+/*
+ * Arguments:
+ *      0: filename of network config file
+ *      1+: filenames or directory names to train on
+ */
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
