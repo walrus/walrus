231ac823b76641b024a53015bc2b8d3dd513d21a
Daniel Clay <dclay.daniel@gmail.com>
2017-04-27 12:33:32 +0100

    Init network io files for saving and loading network configurations to and from file

---
+++ network/src/network-io.cpp
@@ -0,0 +1,14 @@
+#include "network-io.hpp"
+
+/*
+ * Functions for saving and loading network configurations to and from files.
+ */
+
+Network *loadNetwork(std::string filename) {
+    return nullptr;
+}
+
+
+int saveNetwork(std::string filename, Network network) {
+    return 0;
+}
