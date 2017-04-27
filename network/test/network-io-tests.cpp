cfe4873122bd0358f27aa8ee789598ec7e963c9c
Daniel Clay <dclay.daniel@gmail.com>
2017-04-27 12:48:22 +0100

    Init unit test file for network save/load

---
+++ network/test/network-io-tests.cpp
@@ -0,0 +1,28 @@
+ /* Test functions for saving and loading network configurations to and from files. */
+
+#include "../src/network-io.hpp"
+#include "../../lib/catch.hpp"
+
+TEST_CASE("Network configurations can be saved to file and loaded from file") {
+     GIVEN("A suitably configured network") {
+         std::random_device rd;
+         std::mt19937 m_mt(rd());
+         std::uniform_real_distribution<float> test_dist = std::uniform_real_distribution<float>(-1.0f, 1.0f);
+
+         int nin = 8;
+         int nhn = 7;
+         int non = 4;
+
+         float dlr = 0.3;
+         float dm = 0.9;
+         float diwm = 0.5;
+
+         Network network = Network(nin, nhn, non, dlr, dm, diwm);
+
+         THEN("It can be saved to file") {
+             int status_code = saveNetwork("network-test-config.txt", network);
+
+             REQUIRE(status_code == 0);
+         }
+     }
+ }
