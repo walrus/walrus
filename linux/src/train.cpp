3a9b47eb105922c06a82f641b07b361f3b2f8b9c
Daniel Clay <dclay.daniel@gmail.com>
2017-05-04 15:15:43 +0100

    Fix training program

--- linux/src/train.cpp
+++ linux/src/train.cpp
@@ -7,6 +7,8 @@
  *
  * Will read every log file with the _normalised suffix and train the network
  * whose config is stored in network/config/network.txt on the data.
+ *
+ * Must be run from the linux/ directory
  */
 
 #include <iostream>
@@ -16,7 +18,7 @@
 #include "../../network/src/network-io.hpp"
 #include "training-set.hpp"
 
-const std::string CONFIG_FILE_LOCATION =  "../../network/config/network.txt";
+const std::string config_file_location =  "../network/config/network.txt";
 
 void trainSet(std::string filename, Network *network) {
     TrainingSet *set = loadTrainingSet(filename);
@@ -37,18 +39,32 @@ int main(int argc, char * argv[]) {
     }
 
     // Check the network config file exists, and if it doesn't, create it.
-    std::ifstream check_config (CONFIG_FILE_LOCATION);
-    if (!check_config.good()) {
+    std::ifstream check_config (config_file_location);
+    if (!check_config.is_open()) {
         std::cout << "Creating new network\n";
         Network *new_network = new Network(20, 10, 1, 0.3, 0.9, 0.5);
-        saveNetwork(CONFIG_FILE_LOCATION, new_network);
+        std::cout << "About to save network\n";
+        saveNetwork(config_file_location, new_network);
+        std::cout << "Saved new network\n";
     }
 
+    std::cout << "Loading network\n";
     // Load the network
-    Network *network = loadNetwork(CONFIG_FILE_LOCATION);
+    Network *network = loadNetwork(config_file_location);
 
+    std::cout << "Checking training file\n";
+    // Check the training file exists, and if it doesn't, exit
+    std::ifstream check_logfile (argv[1]);
+    if (!check_logfile.good()) {
+        check_logfile.close();
+        std::cout << "Invalid log file\n";
+        return 1;
+    }
+
+    std::cout << "Training\n";
     trainSet(argv[1], network);
 
-    saveNetwork(CONFIG_FILE_LOCATION, network);
+    saveNetwork(config_file_location, network);
+
 }
 
