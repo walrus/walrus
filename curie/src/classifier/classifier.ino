f8d06d120167f07bc1db31fd72212784a68b205e
Daniel Clay <dclay.daniel@gmail.com>
2017-05-08 23:16:43 +0100

    Attempt to include network code (NOT WORKING)

--- curie/src/classifier/classifier.ino
+++ curie/src/classifier/classifier.ino
@@ -1,8 +1,12 @@
 #include "CurieIMU.h"
-#include "lib/network.hpp"
+#include "network.hpp"
+#include "network-io.hpp"
+
+Network *network;
 
 void setup() {
-  Serial.begin(9600); // initialize Serial communication
+  /* Initialise Serial communication */
+  Serial.begin(9600);
   while(!Serial) ;    // wait for serial port to connect.
 
   /* Initialise the IMU */
@@ -12,6 +16,9 @@ void setup() {
   CurieIMU.autoCalibrateAccelerometerOffset(X_AXIS, 0);
   CurieIMU.autoCalibrateAccelerometerOffset(Y_AXIS, 0);
   CurieIMU.autoCalibrateAccelerometerOffset(Z_AXIS, 1);
+
+  /* Initialise the network */
+  network =  new Network(20, 10, 1, 0.3, 0.9, 0.5);
 }
 
 void loop() {}
