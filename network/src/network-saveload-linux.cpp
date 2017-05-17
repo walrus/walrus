0967626daca629699847f7119867e6699c86b313
Daniel Clay <dclay.daniel@gmail.com>
2017-05-17 19:07:52 +0100

    Switch to new (hopefully correct) C++ style network config as preparation for #including on Arduino

--- network/src/network-saveload-linux.cpp
+++ network/src/network-saveload-linux.cpp
@@ -1,3 +1,5 @@
+#include <sstream>
+
 #include "network-saveload-linux.hpp"
 
 /*
@@ -16,38 +18,60 @@ Network_L *loadNetwork(std::string filename) {
     }
 
     // Parse the config data in the first six lines and create the network with the specified configuration
-    int nin = std::stoi(lines[0]);
-    int nhn = std::stoi(lines[1]);
-    int non = std::stoi(lines[2]);
+    int nin = std::stoi(lines[0].substr(26, lines[0].length() - 2));
+    int nhn = std::stoi(lines[1].substr(27, lines[1].length() - 2));
+    int non = std::stoi(lines[2].substr(27, lines[2].length() - 2));
 
-    float lr = std::stof(lines[3]);
-    float m = std::stof(lines[4]);
-    float iwm = std::stof(lines[5]);
+    float lr = std::stof(lines[3].substr(27, lines[3].length()-28));
+    float m = std::stof(lines[4].substr(23, lines[4].length()-24));
+    float iwm = std::stof(lines[5].substr(31, lines[5].length()-32));
 
     Network_L *network = new Network_L(nin, nhn, non, lr, m, iwm);
 
     // Parse the hidden weights
-    int line_num = 6;
+    int line_num = 8;
     std::vector<std::vector<float>> hiddenWeights;
     hiddenWeights.resize(nin+1, std::vector<float>(nhn));
 
     for (int i = 0; i < nin+1; i++) {
-        for (int j = 0; j < nhn; j++) {
-            hiddenWeights[i][j] = std::stof(lines[line_num]);
-            line_num++;
+        std::string line = lines[line_num].substr(5, lines[line_num].length()- 8);
+        float lineWeights[nhn];
+        std::string value;
+        std::istringstream iss(line);
+        int k = 0;
+
+        while (std::getline(iss, value, ',')) {
+            lineWeights[k] = stof(value);
+            k++;
         }
+
+        for (int j = 0; j < nhn; j++) {
+            hiddenWeights[i][j] = lineWeights[j];
+        }
+        line_num++;
     }
 
     // Parse the output weights
-    line_num = 6 + ((nin + 1) * nhn);
+    line_num = 11 + (nin + 1);
     std::vector<std::vector<float>> outputWeights;
     outputWeights.resize(nhn+1, std::vector<float>(non));
 
     for (int i = 0; i < nhn+1; i++) {
-        for (int j = 0; j < non; j++) {
-            outputWeights[i][j] = std::stof(lines[line_num]);
-            line_num++;
+        std::string line = lines[line_num].substr(5, lines[line_num].length()- 8);
+        float lineWeights[non];
+        std::string value;
+        std::istringstream iss(line);
+        int k = 0;
+
+        while (std::getline(iss, value, ',')) {
+            lineWeights[k] = stof(value);
+            k++;
         }
+
+        for (int j = 0; j < non; j++) {
+            outputWeights[i][j] = lineWeights[j];
+        }
+        line_num++;
     }
 
     network->loadWeights(hiddenWeights, outputWeights);
@@ -62,34 +86,44 @@ int saveNetwork(std::string filename, Network_L *network) {
         return 1; // Error code
     }
     // Save main config data
-    config_file << std::to_string(network->getNumInputNodes()) + "\n";
-    config_file << std::to_string(network->getNumHiddenNodes()) + "\n";
-    config_file << std::to_string(network->getNumOutputNodes()) + "\n";
-    config_file << std::to_string(network->getLearningRate()) + "\n";
-    config_file << std::to_string(network->getMomentum()) + "\n";
-    config_file << std::to_string(network->getInitialWeightMax()) + "\n";
+    config_file << "const int numInputNodes = " << std::to_string(network->getNumInputNodes()) + ";\n";
+    config_file << "const int numHiddenNodes = " <<  std::to_string(network->getNumHiddenNodes()) + ";\n";
+    config_file << "const int numOutputNodes = " <<  std::to_string(network->getNumOutputNodes()) + ";\n";
+    config_file << "const float learningRate = " <<  std::to_string(network->getLearningRate()) + ";\n";
+    config_file << "const float momentum = " <<  std::to_string(network->getMomentum()) + ";\n";
+    config_file << "const float initialWeightMax = " <<  std::to_string(network->getInitialWeightMax()) + ";\n";
+    config_file << "\n";
 
     // Save hidden weights
     int nin_plus_one = network->getNumInputNodes() +1;
     int nhn = network->getNumHiddenNodes();
     std::vector<std::vector<float>> hiddenWeights = network->getHiddenWeights();
 
+    config_file << "const float hiddenWeights[numInputNodes +1][numHiddenNodes] PROGMEM = {\n";
     for (int i = 0; i < nin_plus_one; i++) {
-        for (int j = 0; j < nhn; j++) {
-            config_file << std::to_string(hiddenWeights[i][j]) + "\n";
+        config_file << "    { ";
+        for (int j = 0; j < nhn-1; j++) {
+            config_file << std::to_string(hiddenWeights[i][j]) + ", ";
         }
+        config_file << std::to_string(hiddenWeights[i][nhn-1]) << " }, \n";
     }
+    config_file << "};\n";
+    config_file << "\n";
 
     // Save output weights
     int nhn_plus_one = network->getNumHiddenNodes() + 1;
     int non = network->getNumOutputNodes();
     std::vector<std::vector<float>> outputWeights = network->getOutputWeights();
 
+    config_file << "const float outputWeights[numHiddenNodes +1][numOutputNodes] PROGMEM = {\n";
     for (int i = 0; i < nhn_plus_one; i++) {
-        for (int j = 0; j < non; j++) {
-            config_file << std::to_string(outputWeights[i][j]) + "\n";
+        config_file << "    { ";
+        for (int j = 0; j < non-1; j++) {
+            config_file << std::to_string(outputWeights[i][j]) + ", ";
         }
+        config_file << std::to_string(outputWeights[i][non-1]) << " }, \n";
     }
+    config_file << "};\n";
 
     config_file.close();
     return 0;
